a94d2695829ba5762c674dc7a1a621d3cafe80ea
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-09-15 13:04:35 +0900

    sample navbar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import SplitterLayout from 'react-splitter-layout';
 class App extends Component {
   render() {
     return (
-      <SplitterLayout vertical percentage secondaryInitialSize={80}>
-        <div>1</div>
-        <SplitterLayout percentage secondaryInitialSize={66}>
-          <div>2</div>
-          <SplitterLayout percentage secondaryInitialSize={50}>
-            <div>3</div>
-            <div>4</div>
+      <div>
+        <div style={{background: 'red'}}>navbar</div>
+        <div>
+          <SplitterLayout vertical percentage secondaryInitialSize={80}>
+            <div>1</div>
+            <SplitterLayout percentage secondaryInitialSize={66}>
+              <div>2</div>
+              <SplitterLayout percentage secondaryInitialSize={50}>
+                <div>3</div>
+                <div>4</div>
+              </SplitterLayout>
+            </SplitterLayout>
           </SplitterLayout>
-        </SplitterLayout>
-      </SplitterLayout>
+        </div>
+      </div>
     );
   }
 }
